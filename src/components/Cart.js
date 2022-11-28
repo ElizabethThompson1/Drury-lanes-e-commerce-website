56e553c73b4e6ffdc6824f718048f51c556d8ff7
Elizabeth <thompsondeveloper@yahoo.com>
2022-11-27 16:14:07 -0800

    created folders and the set for the e-commerce site.

---
+++ src/components/Cart.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const cartItem = () => {
+    return 
+    <div>cartItem</div>;
+};
+
+export default cartItem;
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const cartContext = () => {
+    return 
+    <div>cartContext</div>;
+};
+
+export default cartContext;
