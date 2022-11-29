ce917444a66deaad393650854eeb283e55c550bc
Elizabeth <thompsondeveloper@yahoo.com>
2022-11-29 15:02:50 -0800

    created a product page and added a link to it that goes to that certain products id/page the added a button on the top both on apper when hovered over or for a cell phone clicked on once

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -7,18 +7,25 @@ import "./product.css"
 
 const Product = ({product}) => {
     const {id,image,price,title,category} = product;
+    console.log(product);
     
     return(
-       <div className="border">
-            <div className="" >
-                {/*image*/}
+        <div>
+            <div className="border">
+                <div className="button-container" >
+                    <button className="button">
+                        <BsPlus/>
+                    </button>  
+                    <Link to={`/product/${id}`}>
+                        <BsEyeFill  className="eye"/>
+                    </Link>
+                </div> 
+                    {/*image*/}
                 <div className="clothes">
                     <img className="image" src={image} alt="clothing"/>
                 </div>
-                {}
-                <div></div>
-            </div>
-       </div> 
+            </div>  
+        </div> 
     );
 };
 
